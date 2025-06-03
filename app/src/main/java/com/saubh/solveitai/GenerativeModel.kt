a9e8c082cf243322af8a6c9b4a0bcb52b9fc8a1c
Saubhagya <saubhagyaranjanbehera64@gmail.com>
2025-06-03 19:09:44 +0530

    feat: Use API key from BuildConfig
    This commit updates the `GenerativeModel` to retrieve the API key from `BuildConfig.apiKey` instead of a hardcoded string.

--- app/src/main/java/com/saubh/solveitai/GenerativeModel.kt
+++ app/src/main/java/com/saubh/solveitai/GenerativeModel.kt
@@ -5,6 +5,6 @@ import com.google.ai.client.generativeai.GenerativeModel
 object GenerativeModel {
     val model : GenerativeModel = GenerativeModel(
         modelName = "gemini-2.0-flash",
-        apiKey = "AIzaSyCfvovPYLVfOxzFDIoxEo6ddoyE92i0sS0"
+        apiKey = BuildConfig.apiKey
     )
 }
